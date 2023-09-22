5e0d95da1ac96b547d87bb764fa6c4c443e82176
Pathologic <m@xim.name>
2023-09-22 16:46:48 +0300

    refactor

--- src/Services/Documents/DocumentCreate.php
+++ src/Services/Documents/DocumentCreate.php
@@ -126,7 +126,7 @@ class DocumentCreate implements DocumentServiceInterface
         if (isset($this->documentData['unpub_date']) && !is_numeric($this->documentData['unpub_date'])) {
             unset($this->documentData['unpub_date']);
         }
-        $document = SiteContent::query()->withTrashed()->create($this->documentData);
+        $document = SiteContent::create($this->documentData);
         $this->documentData['id'] = $document->getKey();
 
         $this->prepareTV();
